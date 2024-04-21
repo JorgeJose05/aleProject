ca91e43b774bcf11b9d0fc6496d4099a78346a45
JorgeJose05 <122548242+JorgeJose05@users.noreply.github.com>
2024-04-21 16:47:04 +0200

    Archivos

---
+++ Jugador.js
@@ -0,0 +1,19 @@
+class Jugador{
+   
+
+    constructor(player, name, race, sex, school, level, hp, mp, fiveEsence ){
+    this.jugador = player;
+    this.nombre = name
+    this.raza =race
+    this.sexo = sex 
+    this.escuela = school 
+    this.nivel = level 
+    this.vida = hp 
+    this.mana = mp 
+    this.esencia = fiveEsence 
+
+   }
+    
+
+    
+}
